00039d2f118cdcdd662dfba5a4f3be3bcc0d75d9
William B. Montini <williambm@gmail.com>
2021-09-22 07:48:19 -0300

    Endpoint de Deleção de categorias

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -45,4 +45,10 @@ public class CategoryResource {
 
         return ResponseEntity.ok(dto);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
